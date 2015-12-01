7d86076c8ac09ca4c95198e5d13ecdd25d26c12f
lrajcula <lrajcula02@gmail.com>
2015-11-30 23:41:10 -0500

    fizzbuzz refactor

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 $( document ).ready(function() {
-for ( newNumber = 1; newNumber <= 100; newNumber++) 
+//user is prompted to input a number
+	var userNumber = +prompt('Please submit a number'); //string is converted to a #
+	fizzbuzz(userNumber); //call the function
+
+//declare the function
+function fizzbuzz (userNumber) {
+	for (newNumber = 1; newNumber <= userNumber; newNumber++) 
 {
-if (newNumber % 15 == 0) 
-	console.log("fizz buzz");
-else if (newNumber % 3 == 0)
-	console.log("fizz");
-else if (newNumber % 5 == 0)
-	console.log("buzz");
-else 
-	console.log(newNumber);
+	if (newNumber % 15 == 0) 
+		$('body').append("fizz buzz <br>");
+	else if (newNumber % 3 == 0)
+		$('body').append("fizz <br>");
+	else if (newNumber % 5 == 0)
+		$('body').append("buzz <br>");
+	else 
+		$('body').append(newNumber + "<br>");
+}
+getUserNumber(); //I need to do something that loops us back to the top
 }
 });
-
